d705247204addaea72258d6fa9927d33c2698e24
Corrwin <CorwinSchuerch@gmail.com>
2024-12-05 13:21:17 -0700

    math assignment done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Assignment a1 = new Assignment("Mike Whisozuki", "Fearmongering");
         Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Curious George", "Addition", "How to Add", "1-5");
+        Console.WriteLine(a2.GetHomeworkList());
+
+        
     }
 }
