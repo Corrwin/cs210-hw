c3152c598ea22d44c05ac297355b060bada76d7f
Corrwin <CorwinSchuerch@gmail.com>
2024-12-05 14:14:58 -0700

    finished methods on word and refrence

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,11 +1,27 @@
 using System;
 
  public class Word{
-    public string _text;
-    public bool _isHidden;
+    private string _text;
+    private bool _isHidden;
 
     public Word(string text){
         _text = text;
         _isHidden = false;
+    }
+
+    public void Hide() {
+        _isHidden = true;
+    }
+    public void Show(){
+        _isHidden = false;
+    }
+    public bool isHidden(){
+        return _isHidden;
+    }
+    public string GetDisplayText(){
+        if (_isHidden == true) {                 //if _isHidden is somehow NULL, this means that it will default to showing that word
+            return "____";
+        }
+        else return _text;
     }
  }
