c8b10f1501b4a8db0fb37266e458fac1729c30d4
Corrwin <CorwinSchuerch@gmail.com>
2024-12-06 14:38:57 -0700

    ok reset everything for file stuff

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,7 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {   
-
+        Comment c1  = new Comment("Testman123", "This video sucks");
+        Console.WriteLine(c1.DisplayComment());
     }
 
 }
