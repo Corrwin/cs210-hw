7831a5619b2ee1dd6f1e2a0ebd10c316dc54a982
Corrwin <CorwinSchuerch@gmail.com>
2024-12-09 15:14:56 -0700

    lots of stupid things happened, but now im back to being able to work

---
+++ foundation/Foundation2/Customer.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Customer{
+    Address _address;
+    string _name;
+
+    public Customer(Address adress, string name){
+        
+    }
+}
--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Address a1 = new Address("1234 Old Man Lane", "Laketown", "Ohio", "USA");
+        Console.WriteLine(a1.InUSA());
+        Console.WriteLine(a1.FullAddress());
     }
 }
