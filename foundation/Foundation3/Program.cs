54ef922cc9c291db7eb1dfa9f97188912be65905
Corrwin <CorwinSchuerch@gmail.com>
2024-12-18 21:21:13 -0700

    all done.

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -6,9 +6,17 @@ class Program
     { 
         CycleActivity c1 = new CycleActivity(40, 12);
         RunActivity r1 = new RunActivity(32, 7);
+        SwimActivity s1 = new SwimActivity(40, 0.8);
 
 
-        Console.WriteLine(r1.GetSummary());
-        Console.WriteLine(c1.GetSummary());
+        List<Activity> a1 = new List<Activity>();
+        a1.Add(c1);
+        a1.Add(r1);
+        a1.Add(s1);
+
+
+        foreach (Activity activity in a1){
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
--- foundation/Foundation3/SwimActivity.cs
+++ foundation/Foundation3/SwimActivity.cs
@@ -1,11 +1,32 @@
 using System;
 
+public class SwimActivity : Activity{
+    private double _laps;
 
-//public class SwimActivity : Activity{
+    public SwimActivity(int time, double distance) :base(time, distance){
+        _time = time;
+        _distance = distance;
+        _today = DateTime.Today;
+        _laps = distance * 20;
+    }
 
 
+    public override double GetSpeed(){
+        double speed = (_distance * _time)/60;
+        return Double.Round(speed, 1);
+        }
+
+    public override double GetPace()
+    {
+        return Double.Round(_time/_distance, 1);
+    }
 
 
+    public override string GetSummary()
+    {
+        string _summary = _today.ToLongDateString() + $" Swimming ({_time} min) - Distance {GetDistance()} kilometers, speed {GetSpeed()} kmph, laps {GetPace()}";
+        return _summary;
 
-    
-//}
+    }
+
+}
